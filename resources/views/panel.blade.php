923b90451544fa75dca6ccbbe4f41e2652ced264
ronnyquispe <rq_ronny@hotmail.com>
2018-08-19 13:46:42 -0500

    template administrativo

---
+++ resources/views/panel.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('content')
+          <!-- Breadcrumbs-->
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="#">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Overview</li>
+          </ol>
+
+    <p>Este apartado aparecerá en la seccioooooooooooooooooón "content".</p>
+
+
+
+
+
+
+@endsection
+
