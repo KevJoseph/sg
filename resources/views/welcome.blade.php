f8b1daeeef712b224087b80a7df7589f4673b947
ronnyquispe <rq_ronny@hotmail.com>
2018-08-19 11:40:36 -0500

    edite vista welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    RONNY
                 </div>
 
                 <div class="links">
