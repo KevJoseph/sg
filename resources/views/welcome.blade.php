4eeb88af50d6662039cf2f1339d9947142bb835a
KevJoseph <kevin.ramos@undc.edu.pe>
2018-08-19 12:05:30 -0500

    edite vista

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    RONNY**
+                    KEVIN
                 </div>
 
                 <div class="links">
